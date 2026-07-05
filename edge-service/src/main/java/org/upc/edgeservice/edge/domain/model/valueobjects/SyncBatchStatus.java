a8c9ffee8c2c96dba929b8ad243c931a85f150c9
Jhon Galvez <jhon.galvezchambi@gmail.com>
2026-07-04 23:08:56 -0500

    feat(edge-service): add domain layer

---
+++ edge-service/src/main/java/org/upc/edgeservice/edge/domain/model/valueobjects/SyncBatchStatus.java
@@ -0,0 +1,8 @@
+package org.upc.edgeservice.edge.domain.model.valueobjects;
+
+public enum SyncBatchStatus {
+    ACCEPTED,
+    PARTIALLY_ACCEPTED,
+    DUPLICATE,
+    REJECTED
+}
---
+++ edge-service/src/main/java/org/upc/edgeservice/edge/domain/model/valueobjects/SyncItemStatus.java
@@ -0,0 +1,7 @@
+package org.upc.edgeservice.edge.domain.model.valueobjects;
+
+public enum SyncItemStatus {
+    RECORDED,
+    DUPLICATE,
+    REJECTED
+}
